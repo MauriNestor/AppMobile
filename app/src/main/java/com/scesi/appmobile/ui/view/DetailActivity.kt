841c6f2a84278807bf3e3629f775398da0ac6ec6
MauriNestor <mauric845apaza@gmail.com>
2024-06-24 16:25:37 -0400

    add dates in IU of detailsActivity in funtion default

--- app/src/main/java/com/scesi/appmobile/ui/view/DetailActivity.kt
+++ app/src/main/java/com/scesi/appmobile/ui/view/DetailActivity.kt
@@ -9,5 +9,31 @@ import com.scesi.appmobile.data.model.Result
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val movie = intent.getParcelableExtra<Result>("movie")
+        // Set the movie details in the views
+        movie?.let {
+            binding.textViewTitle.text = it.title
+            binding.textViewOverview.text = it.overview
+            binding.textViewRating.text = it.vote_average.toString()
+
+            Glide.with(this)
+                .load("${Constantes.IMG_BASE_URL}${it.poster_path}")
+                .into(binding.imageView)
+
+            // Add any other data you find interesting
+            binding.textViewReleaseDate.text = it.release_date
+        }
+
+        // Set the back button to finish the activity
+        binding.buttonBack.setOnClickListener {
+            finish()
+        }
+    }
 }
