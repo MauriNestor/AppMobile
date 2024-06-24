3dd44efb6efd6ac6f62bea0bec8825977615ba0c
MauriNestor <mauric845apaza@gmail.com>
2024-06-24 16:17:50 -0400

    add: xml for details and liked in manifest

--- app/src/main/java/com/scesi/appmobile/ui/view/DetailActivity.kt
+++ app/src/main/java/com/scesi/appmobile/ui/view/DetailActivity.kt
@@ -1,4 +1,13 @@
 package com.scesi.appmobile.ui.view
 
-class DetailActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.scesi.appmobile.core.Constantes
+import com.scesi.appmobile.databinding.ActivityDetailBinding
+import com.scesi.appmobile.data.model.Result
+
+class DetailActivity : AppCompatActivity() {
+
+
 }
