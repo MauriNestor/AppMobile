8a65c2cf575bb1ebdeca28e646f8bddeb3e05d31
MauriNestor <mauric845apaza@gmail.com>
2024-06-24 16:21:28 -0400

    build: pareceable in class Result

--- app/src/main/java/com/scesi/appmobile/data/model/MovieResponse.kt
+++ app/src/main/java/com/scesi/appmobile/data/model/MovieResponse.kt
@@ -1,5 +1,7 @@
 package com.scesi.appmobile.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
 data class MovieResponse(
     val page: Int,
     val results: List<Result>,
@@ -21,4 +23,52 @@ data class Result(
     val video: Boolean,
     val vote_average: Double,
     val vote_count: Int
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readByte() != 0.toByte(),
+        parcel.readString().toString(),
+        parcel.createIntArray()?.toList() ?: emptyList(),
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readDouble(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readByte() != 0.toByte(),
+        parcel.readDouble(),
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeByte(if (adult) 1 else 0)
+        parcel.writeString(backdrop_path)
+        parcel.writeIntArray(genre_ids.toIntArray())
+        parcel.writeInt(id)
+        parcel.writeString(original_language)
+        parcel.writeString(original_title)
+        parcel.writeString(overview)
+        parcel.writeDouble(popularity)
+        parcel.writeString(poster_path)
+        parcel.writeString(release_date)
+        parcel.writeString(title)
+        parcel.writeByte(if (video) 1 else 0)
+        parcel.writeDouble(vote_average)
+        parcel.writeInt(vote_count)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Result> {
+        override fun createFromParcel(parcel: Parcel): Result {
+            return Result(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Result?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
