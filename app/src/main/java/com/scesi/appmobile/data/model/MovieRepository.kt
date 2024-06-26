3d23bfda60f03a0a805fba833cd6fabf94983e9d
MauriNestor <mauric845apaza@gmail.com>
2024-06-26 10:39:02 -0400

    fix: problem with called method Movie Repository

--- app/src/main/java/com/scesi/appmobile/data/model/MovieRepository.kt
+++ app/src/main/java/com/scesi/appmobile/data/model/MovieRepository.kt
@@ -1,12 +1,7 @@
 package com.scesi.appmobile.data.model
 
-import com.scesi.appmobile.core.Constantes
 import com.scesi.appmobile.network.ApiService
 
 class MovieRepository(private val apiService: ApiService) {
-
-    suspend fun getNowPlayingMovies(page: Int): MovieResponse {
-        return apiService.getNowPlayingMovies(Constantes.API_KEY, page = page)
-    }
+    suspend fun getMovies(category: String, page: Int) = apiService.getMovies(category, page)
 }
-
