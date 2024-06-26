d6494b32a40588c278d4a10c2e4b22055ccc6f0f
MauriNestor <mauric845apaza@gmail.com>
2024-06-26 10:35:40 -0400

    feat: add new IU for fragment movie, fix: apiService method

--- app/src/main/java/com/scesi/appmobile/network/ApiService.kt
+++ app/src/main/java/com/scesi/appmobile/network/ApiService.kt
@@ -1,15 +1,16 @@
 package com.scesi.appmobile.network
 
+import com.scesi.appmobile.core.Constantes
 import com.scesi.appmobile.data.model.MovieResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-
-    @GET("movie/now_playing")
-    suspend fun getNowPlayingMovies(
-        @Query("api_key") apiKey: String,
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int
+    @GET("movie/{category}")
+    suspend fun getMovies(
+        @Path("category") category: String,
+        @Query("page") page: Int,
+        @Query("api_key") apiKey: String = Constantes.API_KEY
     ): MovieResponse
 }
