e414e17df11476e5dd6b8a9780214788d60c0246
MauriNestor <mauric845apaza@gmail.com>
2024-06-26 11:26:08 -0400

    refactor: movie adapter using new varaibles

--- app/src/main/java/com/scesi/appmobile/ui/viewmodel/MovieAdapter.kt
+++ app/src/main/java/com/scesi/appmobile/ui/viewmodel/MovieAdapter.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.scesi.appmobile.data.model.Result
 import com.scesi.appmobile.databinding.ItemMovieBinding
 import com.scesi.appmobile.ui.view.DetailActivity
+import com.scesi.appmobile.core.Constantes
 
-class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
 
-    private var movies: List<Result> = emptyList()
+    private var movies = listOf<Result>()
+
+    fun submitList(movieList: List<Result>) {
+        movies = movieList
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -21,22 +28,27 @@ class MovieAdapter : RecyclerView.Adapter<MovieViewHolder>() {
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movies[position]
-        holder.binding.movie = movie
-        holder.binding.executePendingBindings()
+        holder.bind(movie)
+    }
 
-        holder.itemView.setOnClickListener {
-            val context = holder.itemView.context
-            val intent = Intent(context, DetailActivity::class.java).apply {
-                putExtra("movie", movie)
+    inner class MovieViewHolder(private val binding: ItemMovieBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(movie: Result) {
+            binding.movie = movie
+            binding.executePendingBindings()
+
+            val posterUrl = "${Constantes.IMG_BASE_URL}${movie.poster_path}"
+            Glide.with(binding.imageView.context)
+                .load(posterUrl)
+                .into(binding.imageView)
+
+            binding.root.setOnClickListener {
+                val context = binding.root.context
+                val intent = Intent(context, DetailActivity::class.java)
+                intent.putExtra("movie", movie)
+                context.startActivity(intent)
             }
-            context.startActivity(intent)
         }
     }
-
-    fun submitList(movies: List<Result>) {
-        this.movies = movies
-        notifyDataSetChanged()
-    }
 }
-
-class MovieViewHolder(val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root)
