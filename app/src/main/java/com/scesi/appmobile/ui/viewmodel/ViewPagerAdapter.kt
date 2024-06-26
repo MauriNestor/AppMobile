0d0ca7f54303c64cc24af12e940147d9bf95e0e7
MauriNestor <mauric845apaza@gmail.com>
2024-06-26 10:41:43 -0400

    add: class viewPager Adapter with fragments

---
+++ app/src/main/java/com/scesi/appmobile/ui/viewmodel/ViewPagerAdapter.kt
@@ -0,0 +1,11 @@
+package com.scesi.appmobile.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(activity: AppCompatActivity, private val fragments: List<Fragment>) :
+    FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = fragments.size
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
