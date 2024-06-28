1c685d220b4df26118bd291aacf36b22ffdfb305
MauriNestor <mauric845apaza@gmail.com>
2024-06-27 23:16:04 -0400

    add fragment about home and favorites

---
+++ app/src/main/java/com/scesi/appmobile/ui/view/HomeFragment.kt
@@ -0,0 +1,61 @@
+package com.scesi.appmobile.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import com.scesi.appmobile.databinding.FragmentHomeBinding
+import com.scesi.appmobile.ui.viewmodel.MovieViewModel
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: MovieViewModel
+
+    override fun onCreateView(
+        inflater: android.view.LayoutInflater, container: android.view.ViewGroup?,
+        savedInstanceState: Bundle?
+    ): android.view.View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: android.view.View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = requireActivity() as FragmentActivity
+        val fragments = listOf(
+            MovieListFragment.newInstance("now_playing"),
+            MovieListFragment.newInstance("popular"),
+            MovieListFragment.newInstance("upcoming"),
+            MovieListFragment.newInstance("top_rated")
+        )
+
+        val titles = listOf("Now Playing", "Popular", "Upcoming", "Top Rated")
+
+        val adapter = ViewPagerAdapter(activity, fragments)
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = titles[position]
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+class ViewPagerAdapter(
+    fragmentActivity: FragmentActivity,
+    private val fragments: List<Fragment>
+) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int = fragments.size
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
