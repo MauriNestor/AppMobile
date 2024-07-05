388e1b842aa1a6d5a6131e5c41efdf528647264e
MauriNestor <mauric845apaza@gmail.com>
2024-07-05 18:53:17 -0400

    change: xml in activity deleted popularity

--- app/src/main/java/com/scesi/appmobile/ui/view/DetailActivity.kt
+++ app/src/main/java/com/scesi/appmobile/ui/view/DetailActivity.kt
@@ -3,7 +3,7 @@ package com.scesi.appmobile.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.scesi.appmobile.data.model.Result
+import com.scesi.appmobile.data.local.entity.MovieEntity
 import com.scesi.appmobile.databinding.ActivityDetailBinding
 import com.scesi.appmobile.utils.Constantes
 
@@ -16,10 +16,10 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val movie: Result? = intent.getParcelableExtra("movie")
+        val movie: MovieEntity? = intent.getSerializableExtra("movie") as? MovieEntity
         movie?.let {
             binding.movie = it
-            val posterUrl = "${Constantes.IMG_BASE_URL}${it.backdrop_path}"
+            val posterUrl = "${Constantes.IMG_BASE_URL}${it.posterPath}"
             Glide.with(this)
                 .load(posterUrl)
                 .into(binding.imageViewBackdrop)
