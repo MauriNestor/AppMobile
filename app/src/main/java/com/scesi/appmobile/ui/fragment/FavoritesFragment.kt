400d10331a4a12e1ccf037bb9b6e293561b1b790
MauriNestor <mauric845apaza@gmail.com>
2024-07-07 20:51:19 -0400

    add: logic for fragment faavorites and it xml

--- app/src/main/java/com/scesi/appmobile/ui/fragment/FavoritesFragment.kt
+++ app/src/main/java/com/scesi/appmobile/ui/fragment/FavoritesFragment.kt
@@ -5,22 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.scesi.appmobile.MyApplication
 import com.scesi.appmobile.databinding.FragmentFavoritesBinding
+import com.scesi.appmobile.ui.adapter.MovieAdapter
+import com.scesi.appmobile.ui.viewmodel.MovieViewModel
+import com.scesi.appmobile.ui.viewmodel.MovieViewModelFactory
 
 class FavoritesFragment : Fragment() {
-    private var _binding: FragmentFavoritesBinding? = null
-    private val binding get() = _binding!!
+
+    private lateinit var binding: FragmentFavoritesBinding
+    private lateinit var viewModel: MovieViewModel
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val application = requireNotNull(this.activity).application
+        val repository = (application as MyApplication).repository
+        val factory = MovieViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory).get(MovieViewModel::class.java)
+
+        movieAdapter = MovieAdapter()
+
+        binding.recyclerViewFavorites.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = movieAdapter
+        }
+
+        viewModel.favoriteMovies.observe(viewLifecycleOwner, { movies ->
+            movieAdapter.submitList(movies)
+        })
+
+        viewModel.getFavoriteMovies()
     }
 }
